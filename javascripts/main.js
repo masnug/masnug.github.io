869f9e0e8324d32dc1d1f2fd759d1d9efd1d3138
Wuri Nugrahadi <wuri.nugrahadi@live.com>
2014-03-17 16:47:25 +0700

    console style

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,19 @@
-console.log('This would be the main JS file.');
+jQuery(function($){
+    $('.line').width(($(window).width() - 20));
+    $('body').keypress(function( event ) {
+        var text = $('#command').text();
+        if ( event.which == 13 ) {
+            event.preventDefault();
+            var command = text.split(' ')[0];
+            $('#output').append('<div class="line">guest@masnug.github.io:~$ Perintah \''+command+'\' tidak ditemukan!</div>');
+            $('#command').empty();
+        } else {
+          $('#command').append(String.fromCharCode(event.which));
+        }
+    }).keyup(function(event){
+        var text = $('#command').text();
+        if ( event.which == 8 ) {
+            $('#command').html(text.slice(0,-1));
+        }
+    });
+});
